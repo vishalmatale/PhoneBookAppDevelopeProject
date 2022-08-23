3e8042be9bc06cf379789a17597bc5b094b72370
vishalmatale <you@example.com>
2022-08-23 14:54:54 +0530

    PHBADP-8 create Entity class

--- src/main/java/com/BikkadIT/Entity/Contact.java
+++ src/main/java/com/BikkadIT/Entity/Contact.java
@@ -1,5 +1,45 @@
 package com.BikkadIT.Entity;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Data;
+@Entity
+@Data
+@Table(name = "TABLE_CONTACT")
 public class Contact {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CONTACT_ID")
+	private int contactId;
+  
+  @Column(name = "CONTACT_NAME")
+	private String contactName;
+  
+  @Column(name = "CONTACT_NUMBER")
+	private long contactNumber;
+  
+  @Column(name = "ACTIVE_SWITCH")
+	private Character activeSwitch;
+  
+  @Column(name = "CREATE_DATE")
+  @CreationTimestamp
+	private LocalDate CreatedDate;
+  
+  @Column(name = "UPDATE_DATE")
+  @UpdateTimestamp
+	private LocalDate updDate;
+	
+	
+	
 }
