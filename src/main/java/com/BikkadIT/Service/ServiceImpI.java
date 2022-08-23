7e4a82ed7fea547b89f0e4a8b8b91dcb05081a19
vishalmatale <you@example.com>
2022-08-23 15:12:49 +0530

    PHBADP-11 create serviceImpI class and save method

--- src/main/java/com/BikkadIT/Service/ServiceImpI.java
+++ src/main/java/com/BikkadIT/Service/ServiceImpI.java
@@ -1,5 +1,31 @@
 package com.BikkadIT.Service;
 
-public class ServiceImpI {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.BikkadIT.Entity.Contact;
+import com.BikkadIT.Repositery.ContactRepositery;
+@Service
+public class ServiceImpI implements ServiceI{
+
+	@Autowired
+	private ContactRepositery contactRepositery;
+
+	@Override
+	public boolean savedata(Contact contact) {
+		
+		Contact save = contactRepositery.save(contact);
+		
+		if(save!=null) {
+			return true;
+			
+		}
+		return false;
+	}
+	
+	
+	
+	
+	
+	
 }
