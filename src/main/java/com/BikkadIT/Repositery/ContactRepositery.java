1e09b5bfa3944b9cc56323d7aa0f036aaa7e1377
vishalmatale <you@example.com>
2022-08-23 15:01:19 +0530

    PHBADP-9 create Repositery interface

--- src/main/java/com/BikkadIT/Repositery/ContactRepositery.java
+++ src/main/java/com/BikkadIT/Repositery/ContactRepositery.java
@@ -1,5 +1,10 @@
 package com.BikkadIT.Repositery;
 
-public interface ContactRepositery {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.BikkadIT.Entity.Contact;
+@Repository
+public interface ContactRepositery extends JpaRepository<Contact, Integer> {
 
 }
